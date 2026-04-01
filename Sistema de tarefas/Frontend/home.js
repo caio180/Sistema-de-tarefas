c1a2fd5bf2385afadd813a1651c6d58ec2f6762e
caio180 <caio.lima.viana2009@gmail.com>
2026-04-01 14:22:28 -0300

    Escrevendo direto no js e fazendo as primeiras validações

--- Sistema de tarefas/Frontend/home.js
+++ Sistema de tarefas/Frontend/home.js
@@ -1,3 +1,40 @@
-const email =  document.getElementById('email');
-const senha = document.getElementById('senha');
-const botao_cadas = document.getElementById('Criar')
+const tela_cadas = document.getElementById('cadastro');
+
+function tela_cadastro() {
+    tela_cadas.innerHTML = `
+    <h1>Bem vindo ao sistema de tarefas</h1>
+
+    <form id='formCadastro'>
+
+    <h2>Cadastro</h2>
+
+        <input type="text" id="nome" placeholder="Nome completo" required> <br><br>
+        <input type="email" id="email" placeholder="Email" required> <br><br>
+        <input type="password" id="senha" placeholder="Senha" required minlength="6"> <br><br>
+        <input type="password" id="confirmarSenha" placeholder="Confirmar senha" required> <br><br>
+        <button type="submit" id="Criar">Cadastrar-se</button> 
+    </form>
+        
+        `
+    const form = document.getElementById("formCadastro");
+    form.addEventListener("submit", function(evento){
+        evento.preventDefault(); 
+    
+    const nome = document.getElementById('nome').value;
+    const email = document.getElementById('email').value;
+    const senha = document.getElementById('senha').value;
+    const confirmarSenha = document.getElementById('confirmarSenha').value;
+
+    if(nome === '' || email === '' || senha === '' || confirmarSenha === ''){
+        alert('Os campos obrigatórios ão podem ficar fazios');
+        return;
+    }
+    if(senha !== confirmarSenha){
+        alert('As senhas não coincidem');
+        return;
+    }
+ });
+
+}
+
+tela_cadastro()
